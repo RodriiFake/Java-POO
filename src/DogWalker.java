29ad6eabe0927cc6e81e34777f2845fc0e951b8f
RodriiFake <aruquipasergii@gmail.com>
2024-06-21 22:39:02 -0400

    Primer commit: primer commit de POO

---
+++ src/DogWalker.java
@@ -0,0 +1,11 @@
+public class DogWalker {
+    private String name;
+
+    public DogWalker(String name) {
+        this.name = name;
+    }
+
+    public void walk(Dog dog) {
+        System.out.println("I'm walk with " + dog.getName());
+    }
+}
